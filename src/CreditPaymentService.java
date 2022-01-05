1708140a03cadf5c1f7b9073d3383cbb0c7707d2
Andrey Kolotukhin <AndreyKolotukhin@rambler.ru>
2022-01-05 12:40:12 +0300

    new calculation

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,25 +1,13 @@
 public class CreditPaymentService {
-    public int calculate(int amount) {
-        double percent = 9.99; //процентная ставка
-        int creditTermone = 12; //срок кредита, в месяцах
-        double interestRatePerMonthone = percent / 100 / creditTermone;
-        double monthlyPaymentone = amount * (interestRatePerMonthone + (interestRatePerMonthone / (Math.pow(1 + interestRatePerMonthone, creditTermone) - 1)));
-    return (int) monthlyPaymentone;
-    }
+    public int calculate(int creditTerm) {
+       double percent = 9.99; //процентная ставка
+       int amount = 1_000_000; //сумма кредита, в рублях
 
-    public int calculate2(int amounttwo) {
-        double percent = 9.99; //процентная ставка
-        int creditTermtwo = 24; //срок кредита, в месяцах
-        double interestRatePerMonthtwo = percent / 100 / 12;
-        double monthlyPaymenttwo = amounttwo * (interestRatePerMonthtwo + (interestRatePerMonthtwo / (Math.pow(1 + interestRatePerMonthtwo, creditTermtwo) - 1)));
-    return (int) monthlyPaymenttwo;
-    }
+       int creditTermOne = creditTerm; //срок кредита, в месяцах
 
-    public int calculate3(int amountthree) {
-        double percent = 9.99; //процентная ставка
-        int creditTermthree = 36; //срок кредита, в месяцах
-        double interestRatePerMonththree = percent / 100 / 12;
-        double monthlyPaymentthree = amountthree * (interestRatePerMonththree + (interestRatePerMonththree / (Math.pow(1 + interestRatePerMonththree, creditTermthree) - 1)));
-    return (int) monthlyPaymentthree;
+       double interestRatePerMonth = percent / 100 / 12;
+       double monthlyPayment = amount * (interestRatePerMonth + (interestRatePerMonth / (Math.pow(1 + interestRatePerMonth, creditTermOne) - 1)));
+
+       return (int) monthlyPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,13 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        int amount = 1_000_000; //сумма кредита, в рублях
-        int monthlyPayment = service.calculate(1_000_000);
-        int monthlyPaymenttwo = service.calculate2(1_000_000);
-        int monthlyPaymentthree = service.calculate3(1_000_000);
+        int monthlyPayment = service.calculate(12);
+        int monthlyPayment2 = service.calculate(24);
+        int monthlyPayment3 = service.calculate(36);
 
         System.out.println("Срок кредита 1 год." + " Eжемесячный платеж составит: " + monthlyPayment + " рублей.");
-        System.out.println("Срок кредита 2 года." + " Eжемесячный платеж составит: " + monthlyPaymenttwo + " рублей.");
-        System.out.println("Срок кредита 3 года." + " Eжемесячный платеж составит: " + monthlyPaymentthree + " рублей.");
-
+        System.out.println("Срок кредита 2 года." + " Eжемесячный платеж составит: " + monthlyPayment2 + " рублей.");
+        System.out.println("Срок кредита 3 года." + " Eжемесячный платеж составит: " + monthlyPayment3 + " рублей.");
 
     }
 }
