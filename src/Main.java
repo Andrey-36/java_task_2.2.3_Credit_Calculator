0ec3f29f125e92488568fda79fa6e709441a9f30
Andrey-36 <92155825+Andrey-36@users.noreply.github.com>
2022-01-09 09:37:57 +0300

    universal formula. Main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        int monthlyPayment = service.calculate(12);
-        int monthlyPayment2 = service.calculate(24);
-        int monthlyPayment3 = service.calculate(36);
+        int amount = 1_000_000; //сумма кредита, в рублях
+        double percent = 9.99; //процентная ставка
+        
+        int monthlyPayment = service.calculate(12, amount, percent);
+        int monthlyPayment2 = service.calculate(24, amount, percent);
+        int monthlyPayment3 = service.calculate(36, amount, percent);
 
         System.out.println("Срок кредита 1 год." + " Eжемесячный платеж составит: " + monthlyPayment + " рублей.");
         System.out.println("Срок кредита 2 года." + " Eжемесячный платеж составит: " + monthlyPayment2 + " рублей.");
